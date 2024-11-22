fc24b8a0cd2ba90ef9b2fa5fe83c4536d799c7de
eyosidesta <mamesmsdesta@gmail.com>
2024-11-21 23:23:04 -0700

    minor change for test

--- src/Pages/api/contact.ts
+++ src/Pages/api/contact.ts
@@ -1,46 +1,50 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import nodemailer from 'nodemailer';
+// import nodemailer from 'nodemailer';
 
-export default async function contactHandler(req: NextApiRequest, res: NextApiResponse) {
-
-    if (req.method === 'POST') {
-        const { name, email, phone, message } = req.body;
-        if (!name || !email || !phone || !message) {
-            return res.status(400).json({error: 'All fields are required'});
-        }
-
-        try {
-            await sendEmail(name, email, phone, message);
-            return res.status(200).json({message: 'Email Sent Successfully'});
-        } catch(error) {
-            return res.status(500).json({error: error, errorMessage: 'Error sending email'});
-        } 
-    } else {
-        return res.status(405).json({error: 'Method Not Allowed'});
-    }
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  res.status(200).json({message: 'API is working', request: req.method});
 }
 
+// export default async function contactHandler(req: NextApiRequest, res: NextApiResponse) {
 
-async function sendEmail(
-  name: string,
-  email: string,
-  phone: string,
-  message: string
-) {
-  const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      user: process.env.SENDER_EMAIL,
-      pass: process.env.SENDER_EMAIL_KEY,
-    },
-  });
+//     if (req.method === 'POST') {
+//         const { name, email, phone, message } = req.body;
+//         if (!name || !email || !phone || !message) {
+//             return res.status(400).json({error: 'All fields are required'});
+//         }
 
-  const mailOptions = {
-    from: email,
-    to: process.env.RECIEVER_EMAIL,
-    subject: `Home Care Message - FROM ${name}`,
-    text: `Sender's Name: ${name}\nSender's Email is: ${email} \nSender's Phone Number: ${phone} \n\nSenders's Message\n********************\n${message}\n********************`,
-  };
+//         try {
+//             await sendEmail(name, email, phone, message);
+//             return res.status(200).json({message: 'Email Sent Successfully'});
+//         } catch(error) {
+//             return res.status(500).json({error: error, errorMessage: 'Error sending email'});
+//         } 
+//     } else {
+//         return res.status(405).json({error: 'Method Not Allowed'});
+//     }
+// }
 
-  return transporter.sendMail(mailOptions);
-}
+
+// async function sendEmail(
+//   name: string,
+//   email: string,
+//   phone: string,
+//   message: string
+// ) {
+//   const transporter = nodemailer.createTransport({
+//     service: 'gmail',
+//     auth: {
+//       user: process.env.SENDER_EMAIL,
+//       pass: process.env.SENDER_EMAIL_KEY,
+//     },
+//   });
+
+//   const mailOptions = {
+//     from: email,
+//     to: process.env.RECIEVER_EMAIL,
+//     subject: `Home Care Message - FROM ${name}`,
+//     text: `Sender's Name: ${name}\nSender's Email is: ${email} \nSender's Phone Number: ${phone} \n\nSenders's Message\n********************\n${message}\n********************`,
+//   };
+
+//   return transporter.sendMail(mailOptions);
+// }
